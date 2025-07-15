a6ebc84e7af3f5ed3ba1d0c161f0ec6aeee8b046
Om <om@Oms-MacBook-Pro.local>
2025-07-16 04:25:03 +0530

    deploment auth fixed

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -12,7 +12,6 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     const token = getToken();
-    console.log("🔐 Token:", token); // remove later
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
