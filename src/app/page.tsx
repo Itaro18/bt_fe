5d147fd0b0193cc59d383781f4cef8693c86cc98
Om <om@Oms-MacBook-Pro.local>
2025-07-18 00:18:39 +0530

    home styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client"
 import LogoutButton from "@/components/LogoutButton";
-
+import { Poppins } from 'next/font/google';
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-poppins',
+  display: 'swap',
+});
 
 export default function Home() {
 
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen ">
+      <div className="mb-24">
+        <h1 className={`text-4xl text-[#E0E0E0] ${poppins.className}`}>
+          Elite Homestays
+        </h1>
+      </div>
+      
       <LogoutButton/>
     </div>
   );
